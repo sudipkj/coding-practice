674c8f762037dc1d94ff451b3483ffae431ec8ff
Sudipkj <sudip.jana@aeris.net>
2025-03-20 01:58:10 +0530

    Find factorial of a number

---
+++ src/main/java/practice/code/FactorialTest.java
@@ -0,0 +1,17 @@
+package practice.code;
+
+public class FactorialTest {
+
+    public static void main(String[] args) {
+        System.out.println(getFactorialValue(5));
+    }
+
+    public static int getFactorialValue(int n){
+        if(n == 1){
+           return 1 ;
+        }else {
+
+            return n * getFactorialValue(n-1);
+        }
+    }
+}
