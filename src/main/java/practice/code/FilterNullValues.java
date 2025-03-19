22b32eda77dedd6673a0af37cc1c1efa269415d6
Sudipkj <sudip.jana@aeris.net>
2025-03-20 00:01:36 +0530

    Filter null

--- src/main/java/practice/code/FilterNullValues.java
+++ src/main/java/practice/code/FilterNullValues.java
@@ -1,4 +1,16 @@
 package practice.code;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class FilterNullValues {
+
+    public static void main(String[] args) {
+        //WAP filter null values from list of values
+
+        List<String> list = Arrays.asList("Sudip", "Rakesh", null, "Rohit");
+
+        list.stream().filter(Objects::nonNull).forEach(System.out::println);
+    }
 }
