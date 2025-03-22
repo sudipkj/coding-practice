7d9cc680dfa4ead2b1a24fa183fe291708d6cb10
Sudipkj <sudip.jana@aeris.net>
2025-03-22 20:25:40 +0530

    Group by dept and list of employees

--- src/main/java/practice/code/EmployeeTest.java
+++ src/main/java/practice/code/EmployeeTest.java
@@ -1,10 +1,10 @@
 package practice.code;
 
-import org.springframework.util.comparator.Comparators;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EmployeeTest {
     public static void main(String[] args) {
@@ -20,6 +20,12 @@ public class EmployeeTest {
         Comparator<Employee> ct1 = (o1, o2) -> o1.getSalary()> o2.getSalary()? -1 : 1;
 
         empList.stream().sorted(ct1).map(e-> e.getEmpId()).skip(1).findFirst().ifPresent(System.out::println);
+
+        // Group Employees by their dept
+
+        Map<String, List<Employee>> empmap = empList.stream().collect(Collectors.groupingBy(Employee::getDept, Collectors.toList()));
+
+        System.out.println(empmap);
 
 
 
