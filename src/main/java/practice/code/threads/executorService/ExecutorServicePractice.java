dfc9627d0737041d9e56cdb4396d36b76b38cb2d
Sudipkj <sudipkj@gmail.com>
2025-08-27 02:51:26 +0530

    Executor Service practice

--- src/main/java/practice/code/threads/executorService/ExecutorServicePractice.java
+++ src/main/java/practice/code/threads/executorService/ExecutorServicePractice.java
@@ -2,28 +2,37 @@ package practice.code.threads.executorService;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 public class ExecutorServicePractice {
 
-    public static void task1(){
-     System.out.println("task 1 " + Thread.currentThread().getName());
+    public static void task1() {
+        System.out.println("task 1 " + Thread.currentThread().getName());
     }
 
-    public static void task2(){
+    public static void task2() {
+//        ThreadUtils.threadSleep();
         System.out.println("task 2 " + Thread.currentThread().getName());
     }
 
-    public static void task3(){
-        System.out.println("task 3 "  + Thread.currentThread().getName());
+    public static void task3() {
+//        ThreadUtils.threadSleep();
+        System.out.println("task 3 " + Thread.currentThread().getName());
     }
 
-    public static void main(String[] args) {
-        ExecutorService ex = Executors.newFixedThreadPool(3);
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService ex = Executors.newFixedThreadPool(1);
         ex.execute(() -> task1());
         ex.execute(() -> task2());
-        ex.submit(()-> task3());
-        ex.shutdownNow();
+        ex.submit(() -> task3());
+//        List<Runnable> incomepleteThreads = ex.shutdownNow();
+//        incomepleteThreads.stream().forEach(System.out::println);
+        ex.shutdown();
+
+        /*Read only 3 things -- more on shutDown and shutDownNow with awaitTermination*/
+        boolean bool = ex.awaitTermination(1, TimeUnit.MILLISECONDS);
+        System.out.println(bool);
 
     }
 
