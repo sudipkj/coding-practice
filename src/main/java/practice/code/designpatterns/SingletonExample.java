28916ca2f77f343a8626b2a206a80ff49ae9b313
Sudipkj <sudipkj@gmail.com>
2025-09-01 00:21:26 +0530

    Singleton class using Enum

---
+++ src/main/java/practice/code/designpatterns/SingletonExample.java
@@ -0,0 +1,40 @@
+package practice.code.designpatterns;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum SingletonExample {
+    INSTANCE;
+
+    private int value;
+    private Map<String, String> configs;
+
+    SingletonExample() {
+        configs = new HashMap<>();
+        configs.put("Log", "my logs");
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getConfigs(String key) {
+        return configs.get(key);
+    }
+
+    public void setConfigs(String key, String value) {
+        configs.put(key, value);
+    }
+
+    public static void main(String[] args) {
+        SingletonExample singleton = SingletonExample.INSTANCE;
+        singleton.setConfigs("Test", "Value of test");
+
+        System.out.println(singleton.getConfigs("Test"));
+
+    }
+}
