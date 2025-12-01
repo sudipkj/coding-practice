f1e4d122b20895450249d94702f5ddda409e6307
Sudipkj <sudipkj@gmail.com>
2025-12-01 11:48:10 +0530

    November Practice day 5:Revision of lambdas and streams

--- src/main/java/practice/nov/java8/EmployeeQuestionnaires.java
+++ src/main/java/practice/nov/java8/EmployeeQuestionnaires.java
@@ -1,8 +1,6 @@
 package practice.nov.java8;
 
-import java.util.List;
-import java.util.Map;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static practice.nov.java8.EmployeeUtility.getAllEmployeeList;
@@ -12,28 +10,36 @@ public class EmployeeQuestionnaires {
     public static void main(String[] args) {
 
         // List total salary spent by company
-        int totalExpense = getAllEmployeeList().stream().mapToInt(Employee::getSalary).sum();
-        System.out.println("List total salary spent by company : " + totalExpense);
-
-
+        int totalSalry = getAllEmployeeList().stream().mapToInt(Employee::getSalary).sum();
+        System.out.println(totalSalry);
         // List total salary spent by company per department
-        Map<String, Integer> totalExpensePerDept = getAllEmployeeList().stream().collect(Collectors.groupingBy((Employee::getDeptName), Collectors.summingInt(Employee::getSalary)));
-        System.out.println("List total salary spent by company per department : " + totalExpensePerDept);
+        Map<String, Integer> expPerDept = getAllEmployeeList().stream().collect(
+                Collectors.groupingBy((Employee::getDeptName), Collectors.summingInt(Employee::getSalary)));
+        expPerDept.entrySet().forEach(System.out::println);
+
+        //Show average salary per dept
+        Map<String, Double> avSalPerDept = getAllEmployeeList().stream().collect(
+                Collectors.groupingBy((Employee::getDeptName),Collectors.averagingInt(Employee:: getSalary)));
+        System.out.println(avSalPerDept);
+
 
 
         // Show number of employees in Sales
-        long numberOfEmpInSales = getAllEmployeeList().stream().filter(e-> e.getDeptName().equalsIgnoreCase("Sales")).count();
-        System.out.println("Show number of employees in Sales :: "+numberOfEmpInSales);
+        long count = getAllEmployeeList().stream().
+                filter(e->e.getDeptName().equalsIgnoreCase("Sales")).count();
+        System.out.println("Number of  employees in Sales is " + count);
+
 
         // Show department wise employees
-        Map<String, List<Employee>> deptWiseEmployee = getAllEmployeeList().stream().collect(Collectors.groupingBy((Employee::getDeptName),
-                Collectors.toList()));
-        System.out.println("Show department wise employees :: "+deptWiseEmployee);
+        Map<String, List<Employee>> deptWiseEmp = getAllEmployeeList().stream().collect(
+                Collectors.groupingBy((Employee::getDeptName), Collectors.toList()));
 
         // Show highest salary and lowest salary
-        OptionalInt maxSalary = getAllEmployeeList().stream().mapToInt(Employee::getSalary).max();
-        OptionalInt minSalary = getAllEmployeeList().stream().mapToInt(Employee::getSalary).min();
-        System.out.println("Highest salary is "+ maxSalary + " and lowest salary is " + minSalary);
+
+        OptionalInt highestSal = getAllEmployeeList().stream().mapToInt(Employee::getSalary).max();
+        System.out.println(highestSal.getAsInt());
+
+
 
     }
 }
