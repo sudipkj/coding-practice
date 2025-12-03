394b29e3d482b3bc28998560bb427d0b8977efa9
Sudipkj <sudipkj@gmail.com>
2025-12-04 00:53:20 +0530

    November Practice day 5:Maximum Subarray

---
+++ src/main/java/practice/nov/tricky/MaximumSubArrayWithWindow.java
@@ -0,0 +1,23 @@
+package practice.nov.tricky;
+
+public class MaximumSubArrayWithWindow {
+
+    public static void main(String[] args) {
+        int [] arr = {100, 200, 300, 400};
+
+        int k = 2;
+        int max = 0;
+        for(int i = 0; i <= arr.length && k<= arr.length; i++){
+            int sum = arr[i];
+
+            for(int j = i+1; j < k; j++){
+                sum = sum + arr[j];
+                max = Math.max(sum, max);
+
+            }
+            k++;
+        }
+        System.out.println(max);
+
+    }
+}
